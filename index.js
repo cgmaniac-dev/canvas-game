ebb4019b10eea571dda37aafe927ce0221730319
cgmaniac-dev <yongamamkolokotho@outlook.com>
2022-08-03 08:58:09 +0200

    Setting up the enemy class.
    Signed-off-by: cgmaniac-dev <yongamamkolokotho@outlook.com>

--- index.js
+++ index.js
@@ -43,6 +43,28 @@ class Projectile{
     }
 }
 
+class Enemy{
+    constructor(x, y, radius, color, velocity){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color =color
+        this.velocity = velocity
+    }
+
+    draw(){
+        c.beginPath()
+        c.arc(this.x,this.y,this.radius,0,Math.PI * 2,false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update(){
+        this.x = this.x+this.velocity.x
+        this.y = this.y+this.velocity.y
+    }
+}
+
 
 const x = canvas.width/2
 const y = canvas.height/2
@@ -50,6 +72,31 @@ const player = new Player(x,y,30,'blue')
 
 
 const projectiles = []
+const enemies = []
+
+function spawnEnemies(){
+    setInterval(()=>{
+        const radius = Math.random()*(30-4)+4
+        let x
+        let y
+        if (Math.random()<0.5){
+            x = Math.random()<0.5?0-radius:canvas.width+radius
+            y =Math.random()*canvas.height
+            // y = Math.random()<0.5?0-radius:canvas.height+radius
+        }else{
+            x = Math.random()*canvas.width
+            y = Math.random()<0.5?0-radius:canvas.height+radius
+        }
+        const color = 'green'
+        const angle = Math.atan2(canvas.height/2 -y,canvas.width/2-x)
+        const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+        enemies.push(new Enemy(x,y,radius,color,velocity))
+
+    },1000)
+}
 // const projectile = new Projectile(canvas.width/2,canvas.height/2,5,'red',{x:1,y:1})
 
 function animate(){
@@ -57,8 +104,13 @@ function animate(){
     c.clearRect(0,0,canvas.width,canvas.height)
     player.draw()
     projectiles.forEach((projectile)=>{
-        projectile.draw()
         projectile.update()
+        projectile.draw()
+    })
+
+    enemies.forEach((enemy)=>{
+        enemy.update()
+        enemy.draw()
     })
 }
 
@@ -74,5 +126,6 @@ window.addEventListener('click',(e)=>{
 })
 
 animate()
+spawnEnemies()
 
 
